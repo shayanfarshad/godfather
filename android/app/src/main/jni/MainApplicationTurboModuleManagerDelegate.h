bc5a88b76f18d95d8e28ebf3de52df8ddac51701
shayanfarshad <farshad.shayan1996@gmail.com>
2023-12-13 17:16:08 +0330

    last build with package name shayanApp

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/godfather/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lcom/shayanApp/godfather/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
